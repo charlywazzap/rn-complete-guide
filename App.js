8aab01fd0ae27832c295210e6f52638836825110
Carlos Castro <carlos.castro@carlos.castro>
2020-12-25 22:37:45 -0600

    quick intro to flexbox on rn

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.viewInput}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Course Goal"
+        />
+        <Button title="Add" />
+      </View>
+      <View></View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//https://reactnative.dev/docs/flexbox
 const styles = StyleSheet.create({
-  container: {
+  viewInput: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignContent: "center",
+    alignItems: "center"
+  },
+  textInput: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
+    marginBottom: 5,
+    
+  }, 
+  container: {
+    padding: 30
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
