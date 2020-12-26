81f66b5c09492520e72dc2010f32813cef81b6af
Carlos Castro <carlos.castro@carlos.castro>
2020-12-26 01:16:12 -0600

    adding header

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Header } from './components/Header';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-
+      <Header title="Guess My Number" />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
 
   container: {
-    padding: 30
+    padding: 0
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export const Header = props => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.title}>{props.title} </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header:{
+        width: '100%',
+        height: 90,
+        padding: 36,
+        backgroundColor : "#f7287b",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        color: 'black',
+        fontSize: 18
+    }
+})
