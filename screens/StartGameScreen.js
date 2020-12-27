48b793a246d802073a0159e22ef2016e648174ed
Carlos Castro <carlos.castro@carlos.castro>
2020-12-26 21:50:05 -0600

    styled input component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 import { Card } from '../components/Card'
+import { Input } from '../components/Input';
 import Colors from '../constants/colors';
 
 export const StartGameScreen = props => {
@@ -9,7 +10,13 @@ export const StartGameScreen = props => {
             <Text style={styles.title}>Start a New Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a number</Text>
-                <TextInput />
+                <Input style={ styles.input } 
+                    blurOnSubmit 
+                    autoCapitalize='none' 
+                    autoCorrect={false} 
+                    keyboardType="number-pad" 
+                    maxLength={2} 
+                />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}><Button title="Confirm"  color={Colors.accent } onPress={() => {}} /></View>
                     <View style={styles.button}><Button title="Reset" color={Colors.primary} onPress={() => {}} /></View>
@@ -31,6 +38,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'flex-start',
         alignContent: "center",
+    },
+    input: {
+        width: 50,
+        justifyContent:'center',
+        alignContent: 'center'
     },
     title: {
         fontSize: 20,
