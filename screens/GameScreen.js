816c84ea974c13aed9a4dc1cd4255058e36ed27d
Carlos Castro <carlos.castro@carlos.castro>
2020-12-28 17:23:23 -0600

    simple list show of guesses

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { View, Text, StyleSheet, Button, Alert, ScrollView } from "react-native";
 import { NumberContainer } from "../components/NumberContainer";
 import { Card } from "../components/Card";
 import Colors from "../constants/colors";
@@ -13,22 +13,16 @@ const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   const randNum = Math.floor(Math.random() * (max - min)) + min;
-  return randNum === exclude
-    ? generateRandomBetween(min, max, exclude)
-    : randNum;
+  return randNum === exclude ? generateRandomBetween(min, max, exclude) : randNum;
 };
 
 export const GameScreen = (props) => {
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
   const rounds = useRef(0);
-  const [currentGuess, setCurrentGuess] = useState(
-    generateRandomBetween(
-      currentLow.current,
-      currentHigh.current,
-      props.userChoice
-    )
-  );
+  const initGuess = useRef(generateRandomBetween(currentLow.current, currentHigh.current, props.userChoice))
+  const [currentGuess, setCurrentGuess] = useState(initGuess.current);
+  const [pastGuesses, setPassGueses] = useState([initGuess.current])
   // runs after render
   const { userChoice, onGameWon } = props;
   useEffect(() => {
@@ -39,35 +33,26 @@ export const GameScreen = (props) => {
 
   const nextGuessHandler = (direction) => {
     if (isGreaterHintTrue(direction)) {
-      showInvalidAlert(
-        " You know this is wrong..., the number should be lower"
-      );
+      showInvalidAlert(" You know this is wrong..., the number should be lower");
       return;
     }
     if (isLowerHintTrue(direction)) {
-      showInvalidAlert(
-        " You know this is wrong..., the number should be higher"
-      );
+      showInvalidAlert(" You know this is wrong..., the number should be higher");
       return;
     }
     direction === "lower"
       ? (currentHigh.current = currentGuess)
-      : (currentLow.current = currentGuess);
-    const nextNum = generateRandomBetween(
-      currentLow.current,
-      currentHigh.current,
-      currentGuess
-    );
+      : (currentLow.current = currentGuess + 1);
+    const nextNum = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
     setCurrentGuess(nextNum);
     rounds.current++;
+    setPassGueses(listOfGueses => [nextNum,...listOfGueses] )
   };
 
   const showInvalidAlert = (text) =>
     Alert.alert("Sneaky User...,", text, [{ text: "Sorry", style: "cancel" }]);
-  const isGreaterHintTrue = (hint) =>
-    hint === "lower" && currentGuess < userChoice;
-  const isLowerHintTrue = (hint) =>
-    hint === "greater" && currentGuess > userChoice;
+  const isGreaterHintTrue = (hint) => hint === "lower" && currentGuess < userChoice;
+  const isLowerHintTrue = (hint) => hint === "greater" && currentGuess > userChoice;
   const isGuessCorrect = (guess) => guess === userChoice;
 
   return (
@@ -76,10 +61,7 @@ export const GameScreen = (props) => {
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
         <View style={styles.button}>
-          <MainButton
-            onPress={nextGuessHandler.bind(this, "lower")}
-            color={Colors.accent}
-          >
+          <MainButton onPress={nextGuessHandler.bind(this, "lower")} color={Colors.accent}>
             <Ionicons name={"md-remove"} size={24} color={"white"} />
           </MainButton>
         </View>
@@ -89,6 +71,13 @@ export const GameScreen = (props) => {
           </MainButton>
         </View>
       </Card>
+      <ScrollView>
+        {pastGuesses.map((guess,idx) => (
+          <View key={idx}>
+            <Text>{guess}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
