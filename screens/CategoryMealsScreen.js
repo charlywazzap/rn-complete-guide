6ce7e2fc9ba0c917aae7f4ddc944128a13bad855
Carlos Castro <carlos.castro@carlos.castro>
2020-12-29 11:54:49 -0600

    main screens created

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+export const CategoriesMealsScreen = props => {
+    return (<View></View>);
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+export const MealDetailsScreen = props => {
+    return (<View></View>);
+}
+
+const styles = StyleSheet.create({})
