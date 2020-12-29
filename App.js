378352f2fb90cdaba4422dca8326fd0834eafa67
Carlos Castro <carlos.castro@carlos.castro>
2020-12-29 12:05:10 -0600

    basic font loading

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
-
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 
 
 export default function App() {
-
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Meal app</Text>
