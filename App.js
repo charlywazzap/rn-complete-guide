26161cb91dc3f317fbf2819961cb542637a19799
Carlos Castro <carlos.castro@carlos.castro>
2020-12-29 11:28:17 -0600

    fresh start

--- App.js
+++ App.js
@@ -1,70 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
-import { Header } from './components/Header';
-import { GameOver } from './screens/GameOver';
-import { GameScreen } from './screens/GameScreen';
-import { StartGameScreen } from './screens/StartGameScreen';
-import AppLoading from 'expo-app-loading';
-import * as Fonts from 'expo-font';
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
 
-// const fetchFonts = () => {
-//   return Fonts.loadAsync({
-//     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-//     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-//   });
-// }
+
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState(null)
-  const [gameWon, setGameWon] = useState(false)
-  const [rounds, setRounds] = useState(0)
-  const [loaded, setLoaded ] = useState(false)
-  let [fontsLoaded] = Fonts.useFonts({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-
-  if (!fontsLoaded){
-    // startAsync needs promise <AppLoading startAsync={fetchFonts} onFinish={() => {setLoaded(true)}} onError={ () => {console.log('error')}}/>
-    return <AppLoading />
-  }
-  
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber)
-    setRounds(0)
-  }
-
-  const wonGameHandler = (rounds) => {
-    setGameWon(true)
-    setRounds(rounds)
-  }
-
-  
-  const newGameHandler = () => {
-    setRounds(0)
-    setUserNumber(null)
-    setGameWon(false)
-  }
-
-  let content = <StartGameScreen onStartGame={startGameHandler}/>
-
-  if (userNumber) {
-    content = <GameScreen  userChoice={userNumber} onGameWon={wonGameHandler}/>
-  }
-
-  if (gameWon) {
-    content = <GameOver userChoice={userNumber} numOfRounds={rounds} onNewGame={newGameHandler}/>
-  }
-  
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header title="Guess My Number" />
-      {content}
-      
-      <StatusBar style="auto" /> 
+      <Text>Meal app</Text>
     </SafeAreaView>
   );
 }
@@ -74,8 +18,5 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
