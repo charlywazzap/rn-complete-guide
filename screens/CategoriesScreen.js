ab676a4f70949e47ac9c21dcb3ed87dda8e22553
Carlos Castro <carlos.castro@carlos.castro>
2020-12-29 19:40:25 -0600

    sample dummy categories and grid

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES } from '../assets/data/dummy-data'
+
+const renderGridItem = itemdata => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemdata.item.title}</Text>
+    </View>
+  )
+}
 
 export const CategoriesScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button
-        title={"Go to Meals"}
-        onPress={() => {
-          props.navigation.push("CategoryMeals");
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={(item, idx)=> item.id}
+      numColumns={2}
+      data={CATEGORIES}
+      renderItem={
+        renderGridItem
+      }
+    />
   );
 };
 
 const styles = StyleSheet.create({
+  gridItem: {
+    flex:1,
+    margin: 15,
+    height: 150,
+  },
   screen: {
     flex: 1,
     justifyContent: "center",
