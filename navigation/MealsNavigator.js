5bec060d3ac0a32149ad8ace00eb7516b980f128
Carlos Castro <carlos.castro@carlos.castro>
2020-12-30 12:58:04 -0600

    fix: formating on navigators

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,44 +1,42 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { CategoriesScreen } from "../screens/CategoriesScreen";
 import { CategoryMealsScreen } from "../screens/CategoryMealsScreen";
 import { MealDetailsScreen } from "../screens/MealDetailsScreen";
-import { FavoritiesScreen} from '../screens/FavoritiesScreen'
-import Colors from "../constants/colors"
-import { Platform } from 'react-native'
+import { FavoritiesScreen } from "../screens/FavoritiesScreen";
+import Colors from "../constants/colors";
+import { Platform } from "react-native";
 
 const NavConfig = {
-  initialRouteName: 'Categories',
-  defaultNavigationOptions:{
+  initialRouteName: "Categories",
+  defaultNavigationOptions: {
     headerStyle: {
       backgroundColor: Platform.OS == "android" ? Colors.primary : "",
     },
     headerTintColor: Platform.OS == "android" ? "white" : Colors.primary,
-  }
-}
-
- const MealNavigator = createStackNavigator({
-  Categories: {
-    screen: CategoriesScreen,
-    headerTitle: "Meal Categories Priority"
   },
-  CategoryMeals: CategoryMealsScreen,
-  MealDetails: { 
-    screen: MealDetailsScreen,
-  },
-},NavConfig);
+};
 
+const MealNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+      headerTitle: "Meal Categories Priority",
+    },
+    CategoryMeals: CategoryMealsScreen,
+    MealDetails: {
+      screen: MealDetailsScreen,
+    },
+  },
+  NavConfig
+);
 
 const MealsFavNavigator = createBottomTabNavigator({
-    Meals: {
-      screen: MealNavigator
-    },
-    Favorites: FavoritiesScreen
-})
+  Meals: {
+    screen: MealNavigator,
+  },
+  Favorites: FavoritiesScreen,
+});
 
-
-
-export const MealNav = createAppContainer(MealsFavNavigator)
-
-
+export const MealNav = createAppContainer(MealsFavNavigator);
