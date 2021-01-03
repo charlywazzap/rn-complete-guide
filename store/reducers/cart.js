46ab84d94dfb6679a0a16846421cdfd8b7ab4927
charlywazzap <carloscvaladez@gmail.com>
2021-01-02 23:28:43 -0600

    fix(cart store): fix typo on cart state amount

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -12,26 +12,26 @@ export const cartReducer = (state = initialState, action) => {
       const addedProduct = action.product;
       const productPrice = addedProduct.price;
       const productTitle = addedProduct.title;
-
+    
       if (state.items[addedProduct.id]) {
         //already added
         const updatedCartItem = new CartItem(
           state.items[addedProduct.id].quantity + 1,
           productPrice,
           productTitle,
-          state.items[addedProduct.id].price + productPrice
+          state.items[addedProduct.id].sum + productPrice
         );
         return {
           ...state,
           items: { ...state.items, [addedProduct.id]: updatedCartItem },
-          totalAmmount: state.totalAmount + productPrice,
+          totalAmount: state.totalAmount + productPrice,
         };
       }
       const newCartItem = new CartItem(1, productPrice, productTitle, productPrice);
       return {
         ...state,
         items: { ...state.items, [addedProduct.id]: newCartItem },
-        totalAmmount: state.totalAmount + productPrice,
+        totalAmount: state.totalAmount + productPrice,
       };
 
       break;
