40513a55f0378e1eb9879ad5caed5ff6d3d14079
charlywazzap <carloscvaladez@gmail.com>
2021-01-02 20:44:01 -0600

    style: add fonts to header title and back title

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -14,6 +14,13 @@ const ProductNavigator = createStackNavigator({
     headerStyle:{
       backgroundColor: Platform.OS == 'android' ? colors.primary : '',
     },
+    headerTitle:{
+      fontFamily:"open-sans-bold"
+    },
+    headerBackTitleStyle: {
+      fontFamily:"open-sans"
+
+    },
     headerTintColor: Platform.OS == 'android' ? 'white' : colors.primary
   }
 })
