c02516434124b121c68136d6e2ad3fc5354fee8d
charlywazzap <carloscvaladez@gmail.com>
2021-01-02 20:40:14 -0600

    style(Product Details): add styling to product detail

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Image, Button} from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import { useSelector } from 'react-redux'
+import colors from '../../constants/colors'
 
 export const ProductDetailScren = props => {
     const productId = props.navigation.getParam('productId')
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id == productId))
     return (
-      <View>
-        <Text>The Product Detail {selectedProduct.title}</Text>
-      </View>
+      <ScrollView >
+          <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+          <Button color={colors.primary} title="Add To Cart" onPress={() => {}}/>
+          <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+          <Text style={styles.description}>{selectedProduct.description}</Text>
+      </ScrollView>
     );
 }
 
@@ -20,5 +25,19 @@ ProductDetailScren.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
-
+  image:{
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginHorizontal: 30,
+  }
 })
